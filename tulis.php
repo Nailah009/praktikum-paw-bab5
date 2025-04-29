8d541f6eb3cdb896fab7bfb98de5918e82e245d5
Nailah009 <nailahansaria1407@gmail.com>
2025-04-30 04:53:56 +0700

    Delete tulis.php

--- tulis.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Teks yang ingin ditulis
-$isi = "Ini adalah catatan baru.\nBaris kedua catatan.";
-
-// Menulis ke catatan.txt (jika belum ada, file akan dibuat)
-file_put_contents('catatan.txt', $isi);
-
-echo "Berhasil menulis ke catatan.txt.";
-?>
